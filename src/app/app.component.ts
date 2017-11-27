9861cd661bfda8e886006cbaf181cd0157a1ad72
Speros <smisirlakis@gmail.com>
2017-11-26 23:16:50 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Task } from './task';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  newTask: Task;
+
+  constructor() {
+    this.newTask = new Task();
+  }
+
+  onSubmit() {
+    console.log(this.newTask);
+    this.newTask = new Task();
+  }
+
 }
